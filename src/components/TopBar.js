e4b204057a1710beb3aeee31fe95cf4dafc7093d
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-01-01 17:37:46 +0600

    topbar responsiveness

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // Import from the appropriate package, in this case, free-brands-svg-icons
-import { faPhone, faEnvelope, faMapMarker } from '@fortawesome/free-solid-svg-icons';
+import { faPhone, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { faWhatsapp, faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const TopBar = () => {
@@ -39,48 +39,47 @@ const TopBar = () => {
 
 
     return (
-        <div className=" topbar  bg-dark text-light p-2">
+        <div className="topbar text-light p-2">
             <div className="container">
                 <div className="d-flex align-items-center justify-content-between">
                     <div className="d-flex align-items-center">
-                        <div className="mr-3" onClick={handlePhoneClick} style={{ cursor: 'pointer' }}>
-                        <FontAwesomeIcon icon={faPhone}  className="mr-1"/>
-                            <span>{phoneNumber}</span>
+                        <div className="mr-3 d-flex align-items-center" onClick={handlePhoneClick} style={{ cursor: 'pointer' }}>
+                            <FontAwesomeIcon icon={faPhone} className="mr-1" />                
+                            <span className="d-none d-lg-inline">{phoneNumber}</span>
                         </div>
-                        <div className="mr-3" onClick={handleEmailClick} style={{cursor:'pointer'}}>
-                        <FontAwesomeIcon icon={faEnvelope} className="mr-1"/>
-                            <span>{email}</span>
+                        <div className="mr-3 d-flex align-items-center" onClick={handleEmailClick} style={{ cursor: 'pointer' }}>
+                            <FontAwesomeIcon icon={faEnvelope} className="mr-1" />
+                            <span className="d-none d-lg-inline">{email}</span>
                         </div>
-
 
                         <div className="d-flex align-items-center">
                             {/* WhatsApp icon with click handler */}
                             <a href="#" className="text-light mr-3" onClick={handleWhatsappClick}>
-                            <FontAwesomeIcon icon={faWhatsapp} className="mr-1" />
-                                <span>+971528244497</span>
+                                <FontAwesomeIcon icon={faWhatsapp} className="mr-1" />
+                                <span className="d-none d-lg-inline">+971528244497</span>
                             </a>
                         </div>
 
-                        <div>
-                        <FontAwesomeIcon icon={faMapMarker} className="mr-1" />
-                            <span>Dubai</span>
+                        <div className="d-flex align-items-center">
+                        <FontAwesomeIcon icon={faLocationDot} className="mr-1" />
+                            <span className="d-none d-lg-inline">Dubai</span>
                         </div>
                     </div>
                     <div className="d-flex align-items-center">
                         <a href="#" className="text-light mr-3">
-                        <FontAwesomeIcon icon={faFacebook} />
+                            <FontAwesomeIcon icon={faFacebook} />
                         </a>
                         <a href="#" className="text-light mr-3">
-                        <FontAwesomeIcon icon={faTwitter} />
+                            <FontAwesomeIcon icon={faTwitter} />
                         </a>
                         <a href="#" className="text-light">
-                        <FontAwesomeIcon icon={faLinkedin} />
+                            <FontAwesomeIcon icon={faLinkedin} />
                         </a>
                     </div>
                 </div>
             </div>
         </div>
-    );
+    )
 };
 
 export default TopBar;
