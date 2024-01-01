951a90ac05971dabd4fd31a47b3f212a89e34e93
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2024-01-01 18:04:51 +0600

    Topbar css completed

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // Import from the appropriate package, in this case, free-brands-svg-icons
@@ -8,7 +9,7 @@ import { faWhatsapp, faFacebook, faTwitter, faLinkedin } from '@fortawesome/free
 const TopBar = () => {
 
     // FOR PHONE NUMBER CAT
-    const phoneNumber = '123-456-7890'; // Replace with your actual phone number
+    const phoneNumber = '043478818'; // Replace with your actual phone number
 
     const handlePhoneClick = () => {
         window.location.href = `tel:${phoneNumber}`;
@@ -43,36 +44,46 @@ const TopBar = () => {
             <div className="container">
                 <div className="d-flex align-items-center justify-content-between">
                     <div className="d-flex align-items-center">
-                        <div className="mr-3 d-flex align-items-center" onClick={handlePhoneClick} style={{ cursor: 'pointer' }}>
-                            <FontAwesomeIcon icon={faPhone} className="mr-1" />                
+
+                        {/* Left body of the topbar */}
+
+                        <div className="me-3 d-flex align-items-center " onClick={handlePhoneClick} style={{ cursor: 'pointer' }}>
+                            <FontAwesomeIcon icon={faPhone} className="px-2" />                
                             <span className="d-none d-lg-inline">{phoneNumber}</span>
                         </div>
-                        <div className="mr-3 d-flex align-items-center" onClick={handleEmailClick} style={{ cursor: 'pointer' }}>
-                            <FontAwesomeIcon icon={faEnvelope} className="mr-1" />
+                        <div className="me-3 d-flex align-items-center" onClick={handleEmailClick} style={{ cursor: 'pointer' }}>
+                            <FontAwesomeIcon icon={faEnvelope} className="px-2" />
                             <span className="d-none d-lg-inline">{email}</span>
                         </div>
 
-                        <div className="d-flex align-items-center">
+                        <div className="me-3 d-flex align-items-center">
                             {/* WhatsApp icon with click handler */}
-                            <a href="#" className="text-light mr-3" onClick={handleWhatsappClick}>
-                                <FontAwesomeIcon icon={faWhatsapp} className="mr-1" />
+                            <a href="#" className="text-light mr-3" onClick={handleWhatsappClick} >
+                                <FontAwesomeIcon icon={faWhatsapp} className="px-2" />
                                 <span className="d-none d-lg-inline">+971528244497</span>
                             </a>
                         </div>
 
-                        <div className="d-flex align-items-center">
-                        <FontAwesomeIcon icon={faLocationDot} className="mr-1" />
+                        <div className=" me-3 d-flex align-items-center">
+                        <FontAwesomeIcon icon={faLocationDot} className="px-2" />
                             <span className="d-none d-lg-inline">Dubai</span>
                         </div>
                     </div>
-                    <div className="d-flex align-items-center">
-                        <a href="#" className="text-light mr-3">
+
+
+                    {/* Right body of the topbar */}
+                        
+                    <div className="d-flex align-items-center ">
+                        <a href="#" className="text-light me-3">
                             <FontAwesomeIcon icon={faFacebook} />
                         </a>
-                        <a href="#" className="text-light mr-3">
+                        <a href="#" className="text-light me-3">
                             <FontAwesomeIcon icon={faTwitter} />
                         </a>
-                        <a href="#" className="text-light">
+                        <a href="#" className="text-light me-3">
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+                        <a href="#" className="text-light me-3">
                             <FontAwesomeIcon icon={faLinkedin} />
                         </a>
                     </div>
